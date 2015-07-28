148d363610a83d67cadfedd73c2db3edd36be2cb
Nick Chavez <nchavez324@yahoo.com>
2015-07-28 13:59:31 -0700

    More MainActivity cleanup

--- app/src/main/java/com/hackathon/hold/MainActivity.java
+++ app/src/main/java/com/hackathon/hold/MainActivity.java
@@ -1,22 +1,16 @@
 package com.hackathon.hold;
 
-import java.util.Locale;
-
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 import com.hold.bandlayoutapp.R;
@@ -25,8 +19,6 @@ import com.microsoft.band.tiles.TileEvent;
 
 
 public class MainActivity extends ActionBarActivity implements ActionBar.TabListener {
-
-    private boolean mSignedIn;
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -43,39 +35,71 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
      */
     ViewPager mViewPager;
 
-    private BandInstaller mBandInstaller;
+    private BandManager mBandManager;
 
     //This gives us the text on the phone
-    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            if (intent.getAction() == TileEvent.ACTION_TILE_OPENED) {
-                TileEvent tileOpenData = intent.getParcelableExtra(TileEvent.TILE_EVENT_DATA);
-                Log.d("band_console", "Tile open event received\n" + tileOpenData.toString() + "\n\n");
-            } else if (intent.getAction() == TileEvent.ACTION_TILE_BUTTON_PRESSED) {
-                TileButtonEvent buttonData = intent.getParcelableExtra(TileEvent.TILE_EVENT_DATA);
-                if(buttonData.getElementID()==12) {
-                    Log.d("band_console", "Emergency button pressed.\n");
-                }
-                else if(buttonData.getElementID()==21)
-                {
-                    Log.d("band_console", "Pulse sent.\n");
-                    //notify that friends are being contacted
-                }
-            } else if (intent.getAction() == TileEvent.ACTION_TILE_CLOSED) {
-                TileEvent tileCloseData = intent.getParcelableExtra(TileEvent.TILE_EVENT_DATA);
-                Log.d("band_console", "Tile close event received\n" + tileCloseData.toString() + "\n\n");
-            }
-        }
-    };
+    private BroadcastReceiver mMessageReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mSignedIn = false;
+        mMessageReceiver = getBandBroadcastReceiver();
 
+        setUpUI();
+
+        mBandManager = new BandManager(this);
+    }
+
+    public void onResume() {
+        super.onResume();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(TileEvent.ACTION_TILE_OPENED);
+        filter.addAction(TileEvent.ACTION_TILE_BUTTON_PRESSED);
+        filter.addAction(TileEvent.ACTION_TILE_CLOSED);
+        registerReceiver(mMessageReceiver, filter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mMessageReceiver);
+    }
+
+    private BroadcastReceiver getBandBroadcastReceiver()
+    {
+        return new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (intent.getAction() == TileEvent.ACTION_TILE_OPENED) {
+                    TileEvent tileOpenData = intent.getParcelableExtra(TileEvent.TILE_EVENT_DATA);
+                    Log.d("band_console", "Tile open event received\n" + tileOpenData.toString() + "\n\n");
+                } else if (intent.getAction() == TileEvent.ACTION_TILE_BUTTON_PRESSED) {
+                    TileButtonEvent buttonData = intent.getParcelableExtra(TileEvent.TILE_EVENT_DATA);
+                    if(buttonData.getElementID()==12) {
+                        Log.d("band_console", "Emergency button pressed.\n");
+                    }
+                    else if(buttonData.getElementID()==21)
+                    {
+                        Log.d("band_console", "Pulse sent.\n");
+                        //notify that friends are being contacted
+                    }
+                } else if (intent.getAction() == TileEvent.ACTION_TILE_CLOSED) {
+                    TileEvent tileCloseData = intent.getParcelableExtra(TileEvent.TILE_EVENT_DATA);
+                    Log.d("band_console", "Tile close event received\n" + tileCloseData.toString() + "\n\n");
+                }
+            }
+        };
+    }
+
+    public void onInstallApp(View v)
+    {
+        mBandManager.installApp();
+    }
+
+    private void setUpUI()
+    {
         // Set up the action bar.
         final ActionBar actionBar = getSupportActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
@@ -109,56 +133,10 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
                             .setText(mSectionsPagerAdapter.getPageTitle(i))
                             .setTabListener(this));
         }
-
-        mBandInstaller = new BandInstaller(this);
-    }
-
-    public void onResume() {
-        super.onResume();
-        IntentFilter filter = new IntentFilter();
-        filter.addAction(TileEvent.ACTION_TILE_OPENED);
-        filter.addAction(TileEvent.ACTION_TILE_BUTTON_PRESSED);
-        filter.addAction(TileEvent.ACTION_TILE_CLOSED);
-        registerReceiver(mMessageReceiver, filter);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        unregisterReceiver(mMessageReceiver);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void onInstallApp(View v)
-    {
-        mBandInstaller.installApp();
     }
 
     @Override
     public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-        // When the given tab is selected, switch to the corresponding page in
-        // the ViewPager.
         mViewPager.setCurrentItem(tab.getPosition());
     }
 
