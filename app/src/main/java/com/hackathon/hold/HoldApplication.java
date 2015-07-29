1783ed10bc443274a84fcafbd420170e21c98455
Julia Wu <julia_wu@brown.edu>
2015-07-28 21:20:12 -0700

    Added Push Notifications

--- app/src/main/java/com/hackathon/hold/HoldApplication.java
+++ app/src/main/java/com/hackathon/hold/HoldApplication.java
@@ -1,8 +1,13 @@
 package com.hackathon.hold;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.parse.Parse;
+import com.parse.ParsePush;
+import com.parse.SaveCallback;
+
+import java.text.ParseException;
 
 /**
  * Created by nick on 7/27/15.
@@ -22,6 +27,17 @@ public class HoldApplication extends Application {
         Parse.enableLocalDatastore(this);
 
         Parse.initialize(this, HoldApplication.appId, HoldApplication.clientKey);
+
+        ParsePush.subscribeInBackground("", new SaveCallback() {
+            @Override
+            public void done(com.parse.ParseException e) {
+                if (e == null) {
+                    Log.d("com.parse.push", "successfully subscribed to the broadcast channel.");
+                } else {
+                    Log.e("com.parse.push", "failed to subscribe for push", e);
+                }
+            }
+        });
 
     }
 }
